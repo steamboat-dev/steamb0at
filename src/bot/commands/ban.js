f556b59b0e483a01b0085bf0be1174b615c35822
Jack <epicawesomewolfgaming@gmail.com>
2017-04-16 21:24:38 -0400

    banne

--- src/bot/commands/ban.js
+++ src/bot/commands/ban.js
@@ -1,19 +1,20 @@
 async function ban(data) {
-    if(data.msg.mentions.user.size === 0) return data.msg.channel.send('Required usage of `user` is required.');
+    if (data.msg.mentions.users.size === 0) return data.msg.channel.send('Required usage of `user` is required.').then(data.complete).catch(data.err);
     let mem = data.msg.guild.member(data.msg.mentions.users.first())
-    if(!mem) return data.msg.channel.send('Please include a valid member!');
-    if(!data.args) return data.msg.channel.send('Required usage of `reason` is required.')
+    if (!mem) return data.msg.channel.send('Please include a valid member!').then(data.complete).catch(data.err);
+    let reason = data.args.slice(1).join(" ").replace(mem, "")
+    if (!data.args[1]) return data.msg.channel.send('Required usage of `reason` is required.').then(data.complete).catch(data.err);
+    await mem.ban(7).then(data.complete).catch(data.err);
+    data.msg.channel.sendMessage(`:hammer: User \`${mem.user.username}#${mem.user.discriminator}\` was banned for the following reason: \`${reason}\``)
 }
 
 module.exports = {
-    commands: [
-        {
-            trigger: ["ban", "banne"],
-            call: ban,
-            description: "Check the bot's heatbeat and latency.",
-            category: "mod",
-            usage: "ban @user reason",
-            permissions: 0
-        }
-    ]
+    commands: [{
+        trigger: ["ban", "banne"],
+        call: ban,
+        description: "Check the bot's heatbeat and latency.",
+        category: "mod",
+        usage: "ban @user reason",
+        permissions: 0
+    }]
 }
